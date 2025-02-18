cce95781fb32f9502a9b77d4c3a8b36cdbb37e35
Lorena Kneipp Vitale <lorena.vitale@tum.de>
2025-02-18 10:51:51 +0100

    Add inital project

---
+++ EducationalGame/EducationalGame/EducationalGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationalGameApp.swift
+//  EducationalGame
+//
+//  Created by Lorena K. Vitale on 18.02.25.
+//
+
+import SwiftUI
+
+@main
+struct EducationalGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
